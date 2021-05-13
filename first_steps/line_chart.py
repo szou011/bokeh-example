6e9880e268e4572437c46517a7d978c8a424003f
Stephen Zou <zou.stephen@gmail.com>
2021-05-13 13:36:42 +1200

    add renderers

--- first_steps/line_chart.py
+++ first_steps/line_chart.py
@@ -1,10 +1,14 @@
 from bokeh.plotting import figure, show
 
 x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+y1 = [6, 7, 2, 4, 5]
+y2 = [2, 3, 4, 5, 6]
+y3 = [4, 5, 5, 7, 2]
 
-p = figure(title='Simple line example', x_axis_label='x', y_axis_label='y')
+p = figure(title='Multiple glyphs example', x_axis_label='x', y_axis_label='y')
 
-p.line(x, y, legend_label='Temp.', line_width=2)
+p.line(x, y1, legend_label='Temp.', line_color='blue', line_width=2)
+p.vbar(x=x, top=y2, legend_label='Rate', color='red', width=0.5, bottom=0)
+p.circle(x, y3, legend_label='Objects', line_color='yellow', size=12)
 
 show(p)
